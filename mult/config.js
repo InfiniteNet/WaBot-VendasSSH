8aa6907cbabc135a381d6b0d1104b8f11e71c02f
InfiniteNet <117833353+InfiniteNet@users.noreply.github.com>
2023-10-26 18:40:18 -0300

    Atualizar o config.js

--- mult/config.js
+++ mult/config.js
@@ -1,8 +1,8 @@
 config = {
-  dono:"559184035474", //seu número com 55+ddd sem o 9
+  dono:"5511916188418", //seu número com 55+ddd sem o 9
   tempo_teste:"3", //tempo do teste em horas
-  valorLogin: 0.10, //valor do login SSH
-  nomeLoja:"Loja teste", //nome da sua loja virtual
+  valorLogin: 0.30, //valor do login SSH
+  nomeLoja:"InfiniteNet", //nome da sua loja virtual
   linkApp:"LinkAqui", //Link do seu app, playStore/Mediafire
   token_mp:"TokenAqui" //seu token do mercado pago
 }
